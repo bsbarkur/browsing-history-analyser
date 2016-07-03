26227da009c31ece2c4800e613e8923645c8d353
Bharat Shetty <bharat.shetty@gmail.com>
2016-07-03 23:58:08 +0530

    Pylint cleanup

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,61 +1,64 @@
-import sqlite3
-import requests
 import sys
+import sqlite3
 from urllib2 import HTTPError
+import requests
 from bs4 import BeautifulSoup
 
 # Output a big file (csv) or a database where documents
 # are neatly separated, and the following information is available:
 # 1) URL of the document, 2) title of the document.
-def extract_from_url(Url_str):
-    Url = Url_str[1]
-    if Url.startswith('http'):
-        print Url
+
+
+def extract_from_url(url_str):
+    url = url_str[1]
+    if url.startswith('http'):
+        print url
     else:
         return
 
     try:
-        r = requests.get(Url, allow_redirects=False)
-        r.encoding = 'utf-8'
-        print str(r.status_code) + ' - ' + str(r.url)
+        req = requests.get(url, allow_redirects=False)
+        req.encoding = 'utf-8'
+        print str(req.status_code) + ' - ' + str(req.url)
 
-        if r.status_code is not 200:
-            print str(r.url) + ' has a status code of: ' + \
-                str(r.status_code) +  ' omitted from database.'
+        if req.status_code is not 200:
+            print str(req.url) + ' has a status code of: ' \
+                + str(req.status_code) + ' omitted from database.'
 
-        bs_obj = BeautifulSoup(r.text)
+        bs_obj = BeautifulSoup(req.text)
 
-        if hasattr(bs_obj.title, 'string') & (r.status_code == requests.codes.ok):
+        if hasattr(bs_obj.title, 'string') \
+                & (req.status_code == requests.codes.ok):
             try:
                 title = unicode(bs_obj.title.string)
-                if Url.startswith('http'):
+                if url.startswith('http'):
                     if title is None:
                         title = u'Untitled'
-                    for x in bs_obj.find_all(['script', 'style', 'meta', '<!--', ]):
-                        x.extract()
+                    checks = ['script', 'style', 'meta', '<!--']
+                    for chk in bs_obj.find_all(checks):
+                        chk.extract()
                     body = bs_obj.get_text()
-                    title_str = title
                     body_str = body.strip()
 
-                    print str(r.status_code) + ' - ' + title + ' - Committed.'
+                    print str(req.status_code) + ' - ' + title + ' - Committed.'
 
                 if title is None:
                     title = u'Untitled'
-            except HTTPError as e:
+            except HTTPError as error:
                 title = u'Untitled'
             except None:
                 title = u'Untitled'
     # can't connect to the host
     except:
-        e = sys.exc_info()[0]
-        print "Error - %s" % e
+        error = sys.exc_info()[0]
+        print "Error - %s" % error
 
 if __name__ == '__main__':
     # [TODO] Set the firefox path here via config file
-    
+
     # set the profiles path to the Firefox directory
-    profiles_path = ''
-    HISTORY_DB = profiles_path + "places.sqlite"
+    PROFILES_PATH = ''
+    HISTORY_DB = PROFILES_PATH + "places.sqlite"
 
     # connect to the sqlite history database
     db = sqlite3.connect(HISTORY_DB)
